e528fe3ab5c01cb3d75dc84a74d73dc50bb47290
Raphael Luckom <Raphaelluckom@gmail.com>
2015-02-23 21:42:25 -0500

    added decorators that take arguments

--- interior_decoration.py
+++ interior_decoration.py
@@ -40,11 +40,11 @@ def test_decorators():
     """Python has a special syntax for replacing a function with a
     wrapped version of that function. A 'decorator function' is a higher-order
     function that accepts a function as its argument and returns a function.
-    You can then put @<decorator name> above the definition of your function and
-    it will automatically be replaced with a wrapped version. This example shows
-    the use of a decorator for sanitizing input, which is something that might
-    have to be done the same way for a number of otherwise different
-    functions."""
+    You can then put @<decorator name> above the definition of your
+    function and it will automatically be replaced with a wrapped version.
+    This example shows the use of a decorator for sanitizing input, which
+    is something that might have to be done the same way for a number of
+    otherwise different functions."""
     def validate_int_decorator(f):
         def validated_f(*args):
             for arg in args:
@@ -62,3 +62,80 @@ def test_decorators():
 
     with pytest.raises(fill_me_in()):
         add_one('4')
+
+
+def decorators_can_take_arguments():
+    """Sometimes you want to pass an argument to a decorator. A contrived
+    example would be if you wanted a decorator that validated that the input
+    was of a type to be specified individually for each decorated function.
+    There are two ways to do this. The first is a higher-order function that
+    returns the decorator function you want. The second is a class with a call
+    method. We'll start with a higher-order function."""
+
+    def input_must_be_a(arg_type):
+        def decorator(f):
+            def decorated(*args):
+                for arg in args:
+                    if not isinstance(arg, arg_type):
+                        raise TypeError("{} must be type {}".format(arg,
+                                                                    arg_type))
+                return f(*args)
+            return decorated
+        return decorator
+
+    @input_must_be_a(list)
+    def append_one(some_list):
+        return some_list.append(1)
+
+    assert fill_me_in() == append_one([])
+    with pytest.raises(fill_me_in()):
+        append_one('4')
+
+    # Another way to do the same thing is to use a class that implements
+    # a __call__ method. First, we'll demonstrate that ordinary functions
+    # are simply objects with __call__ methods:
+
+    assert fill_me_in() == sum([9, 0])
+    assert fill_me_in() == sum.__call__([9, 0])
+
+    class NewSum(object):
+
+        def __call__(some_list):
+            ret = 0
+            for x in some_list:
+                ret += x
+            return ret
+
+    sum_function = NewSum()
+
+    assert fill_me_in() == sum_function([9, 0])
+    assert fill_me_in() == sum_function.__call__([9, 0])
+
+    # To use a class with a __call__ method as a decorator, implement
+    # the __call__ method as the decorator function, and use the
+    # __init__ function to assign the arguments to the decorator to
+    # instance variables.
+
+    class now_input_must_be_a(object):
+
+        def __init__(self, arg_type):
+            self._arg_type = arg_type
+
+        def __call__(self, f):
+            arg_type = self._arg_type
+
+            def decorated(*args):
+                for arg in args:
+                    if not isinstance(arg, arg_type):
+                        raise TypeError("{} must be type {}".format(arg,
+                                                                    arg_type))
+                return f(*args)
+            return decorated
+
+    @now_input_must_be_a(list)
+    def second_append_one(some_list):
+        return some_list.append(1)
+
+    assert fill_me_in() == second_append_one([])
+    with pytest.raises(fill_me_in()):
+        second_append_one('4')
